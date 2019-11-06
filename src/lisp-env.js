83f345c3a4a874b63c06fd8ce05a137e9e195c54
Jason LIn <linjason920523@gmail.com>
2019-11-07 03:22:04 +0800

    Finish basic of execution environment.

--- src/lisp-env.js
+++ src/lisp-env.js
@@ -9,6 +9,21 @@
     for(var index = 0; index < stringInput.length; index++){
       var currentChar = stringInput[index];
 
+      if(currentChar == "\""){
+        if(valueStack.type == "unknow"){
+          valueStack.type = "string";
+          continue;
+        }else if(valueStack.type == "string"){
+          pushVariableName();
+          continue;
+        }
+      }
+      
+      if(valueStack.type == "string"){
+        valueStack.value += currentChar;
+        continue;
+      }
+
       if(currentChar.trim() == ""){
         pushVariableName();
         continue;
@@ -32,21 +47,6 @@
           type:"paren",
           value:currentChar
         });
-        continue;
-      }
-
-      if(currentChar == "\""){
-        if(valueStack.type == "unknow"){
-          valueStack.type = "string";
-          continue;
-        }else if(valueStack.type == "string"){
-          pushVariableName();
-          continue;
-        }
-      }
-
-      if(valueStack.type == "string"){
-        valueStack.value += currentChar;
         continue;
       }
 
@@ -152,31 +152,231 @@
       }
     }while(DeepScopeCount > 0);
     console.log(scopes)
+    return scopes;
   }
 
-  function exec(code,requireEnv){
-    const parsedSourceCode = code;
+  function exec(code,requireEnv, config){
+    config = config || {};
+    var executeIndexList = ("pointerList" in config? config.pointetList : []);
+    var returnValueList = ("returnList" in config? config.returnList : []);
+    var sysValueList = ("sysList" in config? config.returnList : []);
+    var variablePool = ("variablePool" in config? config.variablePool : []);
 
-    if(!Array.isArray(parsedSourceCode)) return;
-    var scopeEnvs = [
-      requireEnv
-    ];
+    importScopeVariables("$-1", requireEnv);
 
-    var scopeReturnValues = [null]
+    var scopeIndex = 0;
 
-    //initizating scope variables and scope returned value
-    for(var i  = 0; i < parsedSourceCode.length -1; i++){
-      scopeEnvs.push({});
-      scopeReturnValues.push(null);
+    initLists();
+
+    var loopCount = 0;
+
+    while(!isDone(scopeIndex)){
+      loopCount ++;
+      if(loopCount > 30){
+        throw "loop count > 1000"
+        break;
+      }
+
+      console.log({
+        scopeIndex,
+        code:code[scopeIndex],
+        executeIndexList
+      });
+
+      const currentPointer = executeIndexList[scopeIndex].index;
+
+      if(currentPointer == 0 && isVariable(code[scopeIndex].value[0])){ //if is vairbale head and while reading the head
+        var headVariable = getVariable(code[scopeIndex].value[0],scopeIndex,code[scopeIndex].extends); //get head variable name
+        if(headVariable[0] == "undefined") throw "value not found!"
+        var targetVariable =  variablePool[headVariable[0]][headVariable[1]]; //get variable datas
+
+        console.log("check first item!!");
+        console.log({
+          headVariable,
+          targetVariable
+        })
+
+        if(targetVariable.type == "function"){ //if head is a function
+          sysValueList[scopeIndex] = targetVariable.setup(code[scopeIndex],{
+            sys:sysValueList[scopeIndex],
+            env:{ //give the control of executeIndex.
+
+            }
+          }); // run init
+          executeIndexList[scopeIndex].call = headVariable;
+          executeIndexMoveOn(scopeIndex,1); //move forward
+          continue;
+        } 
+      }
+
+      if(isDeeperScopeInside(scopeIndex,currentPointer)){
+        executeIndexMoveOn(scopeIndex,1); //move forward
+        var scopeLink = code[scopeIndex].value[currentPointer]
+        scopeIndex = scopeLink.value;
+        scopeIndex = Number(scopeIndex);
+        console.log("after scopeIndex"+scopeIndex);
+        continue;
+      }
+
+      if(currentPointer >= code[scopeIndex].value.length -1){ //if we scanned all of the value, then run it.
+        const firstItem = code[scopeIndex].value[0];
+
+        if(!isVariable(firstItem)){
+          returnValueList[scopeIndex] = getValues(code[scopeIndex]);
+          scopeIndex = getFatherScope(scopeIndex);
+          console.log(scopeIndex);
+          continue;
+        }
+
+
+
+        var headVariable = getVariable(firstItem,scopeIndex,code[scopeIndex].extends); //get head variable name
+        console.log([firstItem,scopeIndex,code[scopeIndex].extends])
+        console.log(headVariable);
+        
+        if(headVariable[0] == "undefined") throw "value not found!"
+        var targetVariable =  variablePool[headVariable[0]][headVariable[1]]; //get variable datas
+        sysValueList[scopeIndex] = targetVariable.run(code[scopeIndex], { //run it
+          sys: sysValueList[scopeIndex],
+          env: {
+            resetExecuteIndex:()=>{
+              console.log("resetExecuteIndex("+scopeIndex+")");
+              resetExecuteIndex(scopeIndex);
+            },
+            getValues
+          }
+        });
+
+        var returnned = sysValueList[scopeIndex].feedback;
+        returnValueList[scopeIndex] = returnned;
+
+        scopeIndex = getFatherScope(scopeIndex);
+        console.log(scopeIndex);
+        console.log("function runned")
+
+        continue;
+      }
+
+      console.log("counting ending");
+      executeIndexList[scopeIndex].index ++;
+
+      // if(isDeeperScopeInside(scopeIndex)){
+      //   var pointerObj = {
+      //     index: 0
+      //   };
+
+      //   scopeIndex++;
+      //   continue;
+      // }
     }
 
-    var programIndex = 0;
 
-    while(true){
-      var currentScopeIndex = programIndex - parsedSourceCode.length -1;
+    console.log(variablePool);
+    console.log(getValues(code[0].value));
 
+    function initLists(){
+      var initExecuteIndexList = (executeIndexList.length == 0);
+      var initSysValueList = (sysValueList.length == 0);
+      var initReturnValueList = (returnValueList.length == 0);
+
+      for(var index in code){
+        if(initExecuteIndexList)
+          executeIndexList.push({call:[],index:0});
+        if(initSysValueList)
+          sysValueList.push({});
+        if(initReturnValueList)
+          returnValueList.push(null);
+      }
     }
-    
+
+    function getVariable(variableName, scopeId, scopeExtends){
+      // retrurns [scopeID weher's variable exist, variable name]
+      if("$"+String(scopeId) in variablePool && variableName in variablePool[scopeId]){
+        return ["$"+String(scopeId), variableName];
+      }
+      for(var index =0; index < scopeExtends.length; index++){
+        var variableScopeId = "$"+String(scopeExtends[(scopeExtends.length - 1 ) - index]);
+        if(variableScopeId in variablePool && variableName in variablePool[variableScopeId]){
+          return [variableScopeId, variableName]
+        }
+      }
+      return ["undefined", null];
+    }
+
+    function executeIndexMoveOn(scopeId,step){
+      console.log(scopeId);
+      executeIndexList[scopeId].index += step;
+    }
+
+    function importScopeVariables(scopeName, vars){
+      var originVars = variablePool[String(scopeName)] || [];
+      variablePool[scopeName] = Object.assign({},originVars, vars); //merge variables
+    }
+
+    function isDeeperScopeInside(scopeId,index){
+      if(index != undefined){
+        var currentItem = code[scopeId].value[index];
+        console.log({
+          currentItem
+        })
+        return typeof currentItem == "object" && "type" in currentItem && currentItem.type == "scope";
+      }
+      for(var index = 0; index < code[scopeId]; index++){
+        var currentItem = code[scopeId].value[index];
+        if(typeof currentItem == "object" && "type" in currentItem && currentItem.type == "scope"){
+          return true;
+        }
+        return false;
+      }
+    }
+
+    function getFatherScope(scopeId){
+      return code[scopeId].extends[code[scopeId].extends.length-1];
+    }
+
+    function resetExecuteIndex(scopeId){
+      //resetExecuteIndex(...) meaning rerun this line
+      executeIndexList[scopeId] = 0;
+    }
+
+    function getValues(codeList){
+      var result = [];
+      for(var index in codeList){
+        var currentObj = codeList[index];
+        if(isVariable(currentObj)){
+          result.push({
+            type: "variable",
+            value: currentObj
+          });
+        }else if(isNaN(currentObj) && typeof currentObj == "string"){
+          result.push({
+            type: "string",
+            value: currentObj
+          });
+        }else if(typeof currentObj == "object" && "type" in currentObj && currentObj.type == "scope"){
+          result.push({
+            type: "any",
+            value: returnValueList[currentObj.value]
+          });
+        }else{
+          result.push({
+            type: "number",
+            value: currentObj
+          })
+        }
+      }
+      return result;
+    }
+
+    function isVariable(name){
+      if(typeof name != "string") return false;
+      return name.indexOf("$") == 0;
+    }
+
+    function isDone(scopeId){
+      // scopeId = Number(scopeId);
+      return scopeId == -1 || executeIndexList[scopeId] >= code[scopeId].value.length;
+    }
   }
 
   // function uuidv4() {
