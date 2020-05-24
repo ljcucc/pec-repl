032cbe9e99fa40b4775cfc3be646da5cc628e604
Jason Lin <linjason920523@gmail.com>
2020-05-24 18:28:53 +0800

    add lisp.js implements

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-window.addEventListener("load",(()=>{
+var onload = (()=>{
 
   const ShellInput = document.querySelector(".shell-input");
   const shellContent = document.querySelector(".shell");
@@ -6,17 +6,39 @@ window.addEventListener("load",(()=>{
   ShellInput.addEventListener("keydown",(e)=>{
     if(e.key != "Enter") return; // If enter then run
     var commandString = ShellInput.value;
+
+    if(commandString.trim() == "") return;
     ShellInput.value = "";
 
     runCommand(commandString);
   });
 
   function runCommand(command){
-    shellContent.innerHTML += `${command} <br>`
+    shellContent.innerHTML += `<div class="result"> ${command} <br> </div>`
+    window.scrollTo(0, shellContent.offsetHeight); //ajuest psotion of scrolling
 
-    console.log(shellContent.offsetHeight);
-
-    window.scrollTo(0, shellContent.offsetHeight);
+    var parsed = parenthesize(tokenize(command));
   }
 
-}));
+  function loadLibraries(){
+    _get("./os.json", (result)=>{
+      console.log(result);
+    });
+  }
+
+  function _get(url, callback){
+    var xmlHTTP = new XMLHTTPRequest();
+    xmlHTTP.onreadystatechage = ()=>{
+      if(xmlHttp.readyState == 4 && xmlHttp.state == 200)
+        callback(xmlHttp.resonseText);
+    }
+    xmlHttp.open("GET", "./os.json",  true);
+    xmlHttp.send(null);
+  }
+
+  window.loadLibraries = loadLibraries;
+
+  onload = "you can;t access it."; //clean function object
+})
+
+window.addEventListener("load",onload);
