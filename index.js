5492ac8cbdeff6de1791fac80ed04db09d364284
Jason Lin <linjason920523@gmail.com>
2020-05-25 23:31:41 +0800

    add import libraries

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var onload = (()=>{
   const ShellInput = document.querySelector(".shell-input");
   const shellContent = document.querySelector(".shell");
 
+  loadLibraries();
+
   ShellInput.addEventListener("keydown",(e)=>{
     if(e.key != "Enter") return; // If enter then run
     var commandString = ShellInput.value;
@@ -14,29 +16,50 @@ var onload = (()=>{
   });
 
   function runCommand(command){
-    shellContent.innerHTML += `<div class="result"> ${command} <br> </div>`
+    shellContent.innerHTML += `<div class="result"> > ${command} <br> </div>`
     window.scrollTo(0, shellContent.offsetHeight); //ajuest psotion of scrolling
 
     var parsed = parenthesize(tokenize(command));
+    var compiled_code = run_lisp(parsed);
+    try{
+      eval(compiled_code);
+    }catch(e){
+      shellContent.innerHTML += `<div class="result error"> ${e.name} </div>`
+    }
   }
 
   function loadLibraries(){
     _get("./os.json", (result)=>{
+      result = JSON.parse(result);
       console.log(result);
+      add_gfunc(result);
     });
   }
 
   function _get(url, callback){
-    var xmlHTTP = new XMLHTTPRequest();
-    xmlHTTP.onreadystatechage = ()=>{
-      if(xmlHttp.readyState == 4 && xmlHttp.state == 200)
-        callback(xmlHttp.resonseText);
-    }
-    xmlHttp.open("GET", "./os.json",  true);
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.addEventListener("load", ()=>{
+      callback(xmlHttp.responseText);
+    });
+    xmlHttp.open("GET", "./basic.json",  true);
     xmlHttp.send(null);
+
+    console.log(xmlHttp);
+  }
+
+  function clear(){
+    shellContent.innerHTML = "";
+  }
+
+  function print(msg){
+        shellContent.innerHTML += `<div class="result"> ${msg} <br> </div>`
   }
 
   window.loadLibraries = loadLibraries;
+  window.shell = {
+    clear: ()=> clear(),
+    print: (msg)=> print(msg)
+  };
 
   onload = "you can;t access it."; //clean function object
 })
